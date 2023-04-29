18b199c660843ba7d868b7d0662c012052c9f61c
Oskari Lindroos <oskarilindroos00@gmail.com>
2023-04-29 15:16:27 +0300

    Added some test code for the Event class

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace csharp_days
+﻿using NodaTime;
+using NodaTime.Extensions;
+
+namespace csharp_days
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var events = new List<Event>
+            {
+                new Event(new LocalDate(2023, 4, 30) ,"Birthday", "My birthday"),
+                new Event(new LocalDate(2023, 4, 29) ,"Birthday", "My birthday"),
+                new Event(new LocalDate(2010, 3, 30) ,"Birthday", "My birthday"),
+                new Event(new LocalDate(2026, 4, 29) ,"Birthday", "My birthday"),
+            };
+            
+            events.Sort((e, other) => e.Date.CompareTo(other.Date));
+            foreach (var e in events)
+            {
+               Period difference = Period.Between(e.Date, DateTime.Now.ToLocalDateTime().Date);
+               Console.WriteLine($"{e} -- {e.getDifferenceString(difference)}");
+            }
         }
     }
 }
